b8828887f3c7bfacb92d451e523082b043315026
DarCkDev <201709713@est.umss.edu>
2025-05-13 20:40:45 -0400

    fix: tests modificados de acuerdo a LoginResponseDto

--- py-skillz-backend/src/modules/auth/auth.service.spec.ts
+++ py-skillz-backend/src/modules/auth/auth.service.spec.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { UnauthorizedException } from '@nestjs/common';
-// import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { Role } from '../user/entities/role.entity';
 import { I18nService } from 'nestjs-i18n';
@@ -67,7 +66,8 @@ describe('AuthService', () => {
         dto.password,
       );
       expect(result).toEqual({
-        email: mockUser.email,
+        fullName: mockUser.fullName,
+        role: mockUser.role,
         token: 'jwt-token',
       });
     });
