4ea827db51ee3da6991bcac467a1bb0aefa8dc29
DarCkDev <201709713@est.umss.edu>
2025-05-30 17:26:46 -0400

    subir archivos, eliminar y obtener uno

--- py-skillz-backend/src/modules/upload/upload.service.ts
+++ py-skillz-backend/src/modules/upload/upload.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import * as fs from 'node:fs';
 import { FileType } from './upload.filetype';
 import { FileCreateUploadDto } from './dto/file-create-upload.dto';
@@ -7,12 +12,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Upload } from './entities/upload.entity';
 import { Repository } from 'typeorm';
 import { Source } from './entities/source.entity';
+import { I18nContext, I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class UploadService {
   constructor(
     @InjectRepository(Upload)
     private readonly uploadRepository: Repository<Upload>,
+    private readonly i18n: I18nService,
   ) {}
 
   async handleUpload(
@@ -30,12 +37,29 @@ export class UploadService {
       return { ...saved };
     }
 
-    if (!file) throw new BadRequestException('No file provided');
+    if (!file)
+      throw new BadRequestException({
+        statusCode: 400,
+        message: [
+          this.i18n.t('upload.invalidFile', {
+            lang: I18nContext.current()?.lang,
+          }),
+        ],
+        error: 'Bad Request',
+      });
 
     const mimeValid = this.validateMimeType(dto.type, file.mimetype);
     if (!mimeValid) {
       await fs.promises.unlink(file.path);
-      throw new BadRequestException(`Invalid mime type ${dto.type}`);
+      throw new BadRequestException({
+        statusCode: 400,
+        message: [
+          this.i18n.t('upload.invalidMimeType', {
+            lang: I18nContext.current()?.lang,
+          }),
+        ],
+        error: 'Bad Request',
+      });
     }
 
     const filePath = file.path.replace('public/', '');
@@ -50,6 +74,79 @@ export class UploadService {
     const saved = await this.uploadRepository.save(entity);
 
     return { ...saved };
+  }
+
+  async getOne(id: string): Promise<Upload> {
+    if (!this.isUUID(id)) {
+      throw new BadRequestException({
+        statusCode: 400,
+        message: [
+          this.i18n.t('upload.invalidId', {
+            lang: I18nContext.current()?.lang,
+          }),
+        ],
+        error: 'Bad Request',
+      });
+    }
+    const upload = await this.uploadRepository.findOne({ where: { id } });
+
+    if (!upload) {
+      throw new NotFoundException({
+        statusCode: 404,
+        message: [
+          this.i18n.t('upload.fileNotFound', {
+            lang: I18nContext.current()?.lang,
+          }),
+        ],
+        error: 'Not Found',
+      });
+    }
+    return upload;
+  }
+
+  async delete(id: string): Promise<void> {
+    if (!this.isUUID(id)) {
+      throw new BadRequestException({
+        statusCode: 400,
+        message: [
+          this.i18n.t('upload.invalidId', {
+            lang: I18nContext.current()?.lang,
+          }),
+        ],
+        error: 'Bad Request',
+      });
+    }
+    const upload = await this.uploadRepository.findOne({ where: { id } });
+    if (!upload)
+      throw new NotFoundException({
+        statusCode: 404,
+        message: [
+          this.i18n.t('upload.fileNotFound', {
+            lang: I18nContext.current()?.lang,
+          }),
+        ],
+        error: 'Not Found',
+      });
+
+    if (upload.source === Source.UPLOAD) {
+      try {
+        await fs.promises.unlink(upload.url);
+      } catch (err) {
+        if ((err as NodeJS.ErrnoException).code !== 'ENOENT') {
+          throw new InternalServerErrorException({
+            statusCode: 500,
+            message: [
+              this.i18n.t('upload.errorDeletingFile', {
+                lang: I18nContext.current()?.lang,
+              }),
+            ],
+            error: 'Internal Server Error',
+          });
+        }
+      }
+    }
+
+    await this.uploadRepository.delete(id);
   }
 
   private validateMimeType(type: FileType, mimetype: string): boolean {
@@ -69,5 +166,11 @@ export class UploadService {
       ],
     };
     return mimeMap[type]?.includes(mimetype) ?? false;
+  }
+
+  private isUUID(uuid: string): boolean {
+    const regexExp =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/gi;
+    return regexExp.test(uuid);
   }
 }
