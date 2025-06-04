f9b1c19017a6e86b8507ddb7e323a0eb5f7730c5
adruRR <201809453@est.umss.edu>
2025-06-04 01:14:31 -0400

    FormData

--- py-skillz-frontend/src/features/teacher/api/api.tsx
+++ py-skillz-frontend/src/features/teacher/api/api.tsx
@@ -1,18 +1,14 @@
-import { CursoData } from '../types';
-
 const API_URL = 'http://localhost:3003'; // Cambia si tu backend está en otra URL
 
-export const crearCurso = async (payload: CursoData) => {
+export const crearCurso = async (formData: FormData) => {
   //const token = localStorage.getItem('token'); // Asegúrate de que el token esté almacenado en el localStorage
   const token = sessionStorage.getItem('token'); // O usa sessionStorage si prefieres
   const response = await fetch(`${API_URL}/curso`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      // Si usas autenticación, agrega el token aquí
       'Authorization': `Bearer ${token}`,
     },
-    body: JSON.stringify(payload),
+    body: formData,
   });
   if (!response.ok) throw new Error('Error al crear el curso');
   return await response.json();
